58e6d1cf87fc919815550a633b401ce95c1ee3b9
rubanovigor <rubanovigor@gmail.com>
2015-03-01 21:07:11 -0300

    build first graph

--- mentalStatesDistribution.R
+++ mentalStatesDistribution.R
@@ -6,35 +6,30 @@ rm(list=ls());
 library(cluster); library(ggplot2); library(gridExtra); library(scatterplot3d);
 library(reshape2)
 # --===========================================================================
-# csv_dir = "~/aiworker.com.R/data/csv"
-# csv_dir = "~/aiworker.com.R/data/csv/alex"
-csv_dir = "~/aiworker.com.R/data/csv/exosphere"
-function_dir = "~/aiworker.com.R/function"
-# -- 
-setwd(function_dir); source("csvDataProcessing.R")
-eegData = csvDataProcessing(csv_dir)
+# # csv_dir = "~/aiworker.com.R/data/csv"
+# # csv_dir = "~/aiworker.com.R/data/csv/alex"
+# csv_dir = "~/aiworker.com.R/data/csv/exosphere"
+# function_dir = "~/aiworker.com.R/function"
+# # -- 
+# setwd(function_dir); source("csvDataProcessing.R")
+# eegData = csvDataProcessing(csv_dir)
+# save(eegData, file = "~/aiworker.com.R/data/exosphere.RData")
+load("~/aiworker.com.R/data/exosphere.RData")
 
-# -- parsing and formatting time and location
-# data.frame(do.call('rbind', strsplit(as.character(eegData$time[1]),'T',fixed=TRUE)))
-# substr(as.character(eegData$time[1]),1,13)
-
-# --
+# --formatting and fixing initial data
 df = eegData; 
 levels(df$CurrentActivity) <- c(levels(df$CurrentActivity), "football") 
 df$CurrentActivity[df$CurrentActivity=="basketball"] = "football";
 levels(df$username) <- c(levels(df$username), c("Gerson","Moritz")) 
 df$username[df$username=="AntonioManno"] = "Gerson";
 df$username[df$username=="SkinnerLayne"] = "Moritz";
-
+    # -- adding empty records
 temp = df[1:50,]; temp[,] = NA
 temp$username= "EzequielDjeredjian"; temp$CurrentActivity = "football" 
 temp$att = 50; temp$med = 50;
 df = rbind(df, temp)
 remove(temp)
-# tt$CurrentActivity = 'notDSC' ;
-# tt2$CurrentActivity = 'tDSC' ;
-# df = rbind(tt,tt2); 
-
+    # -- getting waves name from initial data
 wave_name = colnames(df[,c(6:13)])
 # -- clean up rawData
 # for(i in seq(6,13,1)) {
@@ -44,134 +39,150 @@ wave_name = colnames(df[,c(6:13)])
 #   pat = df[,wave_name[j]] > p95 | df[,wave_name[j]] < p05
 #   df[ pat ,wave_name[j]] = NA
 # }  
+    # -- creating S index
 df$s = df$att-df$med
-# -- remove ZERO values
+    # -- remove ZERO values
 df <- df[ which(df$med!=0 & df$att!=0), ]
-
-# -- remove unused Acticity, e.g. 'default'
+    # -- remove unused Acticity, e.g. 'default'
 # df <- df[ which(df$CurrentActivity!='default'), ]
 
-# -- manipulate with username column
-# df$username = 'test'
-
-# # df_waves_long = melt(dt[,c(6:13)]); 
-# df_waves_long = melt(df[,c(4,5,6:13,14)], id.vars = c("att", "med", "CurrentActivity"),
-#                                           variable.name = "waves", value.name = "value"); 
-
-# df_waves_long = melt(df[,c(4,5,8,14)], id.vars = c("att", "med", "CurrentActivity"),
-#                                        variable.name = "waves", value.name = "value"); 
-
-# --=============================
+# -- plot only aggregate Distribution
+TotalFlag = F; AggregationFlag = F;
+# =============================
+# =============================
 # -- plotting distribution of Att
-df_waves_long = melt(df[,c(2,4,14)], id.vars = c("username", "CurrentActivity"),
-                     variable.name = "index", value.name = "Att"); 
+gTitle = "EEG: Att Distribution"; yScales = "free_y";
+if(AggregationFlag){
+  meltVar = c("CurrentActivity","att"); 
+  GraphRow = 4; GraphCol = 1; TitletxtSize = 16;  GraphWidth = 800;
+  xVar = c("CurrentActivity"); xFacetWrap = ~CurrentActivity;
+}else{
+  meltVar = c("username","CurrentActivity","att"); 
+  GraphRow = 4; GraphCol = 3; TitletxtSize = 9;  GraphWidth = 1200;
+  xVar = c("username", "CurrentActivity"); xFacetWrap = username~CurrentActivity;
+}
+
+    # -- transforming df to plot using face_wrap
+df_waves_long = melt(df[,meltVar], id.vars = xVar, variable.name = "index", value.name = "att");
+
+if(AggregationFlag){
+  df_waves_long_Total = df[,meltVar]; df_waves_long_Total$index = 'att'; df_waves_long_Total$CurrentActivity = 'aggregate';
+  df_waves_long_Total = df_waves_long_Total[c(1,3,2)]
+  df_waves_long = rbind(df_waves_long, df_waves_long_Total) 
+}
+if(TotalFlag){df_waves_long = df_waves_long_Total}
+
     # -- remove Att=0
-# df_waves_long$Att[df_waves_long$Att==0] = NA;  df_waves_long = na.omit(df_waves_long)
-# df_waves_long[df_waves_long==0] = NA;  df_waves_long = na.omit(df_waves_long)
+# df_waves_long$att[df_waves_long$att==0] = NA;  df_waves_long$att = na.omit(df_waves_long$att)
 
-yScales = "free_y"; gTitle = "EEG Att distribution";
     # -- crete Att distribution graph
-p1 = ggplot(df_waves_long, aes(x=Att)) +
-  geom_histogram(colour="white") +  facet_wrap(username~CurrentActivity, scales = yScales, ncol=3) +
-  ggtitle(gTitle) +  theme(legend.position="none") 
-
-# --=============================
-# -- plotting distribution of Med
-df_waves_long = melt(df[,c(2,5,14)], id.vars = c("username","CurrentActivity"),
-                     variable.name = "index", value.name = "Med"); 
-# -- remove Att=0
-df_waves_long[df_waves_long==0] = NA;  df_waves_long = na.omit(df_waves_long)
-yScales = "free"; 
-gTitle = "EEG Med distribution";
-p2 = ggplot(df_waves_long, aes(x=Med)) +
-  geom_histogram(colour="white") +  facet_wrap(username~CurrentActivity, scales = yScales, ncol=4) +
-  ggtitle(gTitle) +  theme(legend.position="none") 
-
-# --=============================
-# -- plotting distribution of S 
-df_waves_long = melt(df[,c(14,15)], id.vars = c("CurrentActivity"),
-                     variable.name = "index", value.name = "S");
-
-    # -- remove S=0
-df_waves_long[df_waves_long==0] = NA;  df_waves_long = na.omit(df_waves_long)
-
-yScales = "free"; 
-gTitle = "EEG Att-Med distribution";
-p3 = ggplot(df_waves_long, aes(x=S)) +
-  geom_histogram(colour="white") +  facet_wrap(~CurrentActivity, scales = yScales, nrow=1) +
-  geom_vline(aes(xintercept=-30), colour="blue",linetype="solid",size=0.5) +
-  geom_vline(aes(xintercept=0), colour="red",linetype="solid",size=1) +
+p1 = ggplot(df_waves_long, aes(x=att)) + geom_histogram(colour="white") +
+  facet_wrap(xFacetWrap, scales = yScales, nrow = GraphRow, ncol = GraphCol) +
   geom_vline(aes(xintercept=30), colour="blue",linetype="solid",size=0.5) +
-  ggtitle(gTitle) +  theme(legend.position="none") 
+  geom_vline(aes(xintercept=50), colour="red", linetype="solid",size=1) +
+  geom_vline(aes(xintercept=70), colour="blue",linetype="solid",size=0.5) +
+  ggtitle(gTitle) + 
+  theme(legend.position="none", 
+        plot.title = element_text(lineheight=1,size=18, face="bold"),
+        strip.text.x = element_text(size=TitletxtSize, face="bold"))
+
+    # -- saving graph to png file
+png("~/aiworker.com.R/output/att_distribution.png", width=GraphWidth, height=800, res=120)
+p1
+dev.off()
+
+# =============================
+# =============================
+# -- plotting distribution of Med
+gTitle = "EEG: Med Distribution"; yScales = "free_y"; 
+if(AggregationFlag){
+  meltVar = c("CurrentActivity","med");
+  GraphRow = 4; GraphCol = 1; TitletxtSize = 16; GraphWidth = 800;
+  xVar = c("CurrentActivity"); xFacetWrap = ~CurrentActivity;
+}else{
+  meltVar = c("username","CurrentActivity","med"); 
+  GraphRow = 4; GraphCol = 3; TitletxtSize = 9; GraphWidth = 1200;
+  xVar = c("username", "CurrentActivity"); xFacetWrap = username~CurrentActivity;
+}
+
+# -- transforming df to plot using face_wrap
+df_waves_long = melt(df[,meltVar], id.vars = xVar, variable.name = "index", value.name = "med");
+
+if(AggregationFlag){
+  df_waves_long_Total = df[,meltVar]; df_waves_long_Total$index = 'med'; df_waves_long_Total$CurrentActivity = 'aggregate';
+  df_waves_long_Total = df_waves_long_Total[c(1,3,2)]
+  df_waves_long = rbind(df_waves_long, df_waves_long_Total)
+}
+if(TotalFlag){df_waves_long = df_waves_long_Total}
+
+# -- remove med=0
+# df_waves_long$med[df_waves_long$med==0] = NA;  df_waves_long$med = na.omit(df_waves_long$med)
+
+# -- crete med distribution graph
+p2 = ggplot(df_waves_long, aes(x=med)) + geom_histogram(colour="white") +
+  facet_wrap(xFacetWrap, scales = yScales, nrow = GraphRow, ncol = GraphCol) +
+  geom_vline(aes(xintercept=30), colour="blue",linetype="solid",size=0.5) +
+  geom_vline(aes(xintercept=50), colour="red", linetype="solid",size=1) +
+  geom_vline(aes(xintercept=70), colour="blue",linetype="solid",size=0.5) +
+  ggtitle(gTitle) + 
+  theme(legend.position="none", 
+        plot.title = element_text(lineheight=1,size=18, face="bold"),
+        strip.text.x = element_text(size=TitletxtSize, face="bold"))
+
+# -- saving graph to png file
+png("~/aiworker.com.R/output/med_distribution.png", width=GraphWidth, height=800, res=120)
+p2
+dev.off()
 
 
-grid.arrange(p1,p2,p3, ncol=1, nrow=3)
+# =============================
+# =============================
+# -- plotting distribution of S
+gTitle = "EEG: S Distribution"; yScales = "free_y";
+if(AggregationFlag){
+  meltVar = c("CurrentActivity","s"); 
+  GraphRow = 4; GraphCol = 1; TitletxtSize = 16;  GraphWidth = 800;
+  xVar = c("CurrentActivity"); xFacetWrap = ~CurrentActivity;
+}else{
+  meltVar = c("username","CurrentActivity","s"); 
+  GraphRow = 4; GraphCol = 3; TitletxtSize = 9;  GraphWidth = 1200;
+  xVar = c("username", "CurrentActivity"); xFacetWrap = username~CurrentActivity;
+}
 
-# df_waves_long = na.omit(df_waves_long)
-# wavesHIST <- 
-# #     ggplot(df_waves_long, aes(x=df_waves_long$value, fill=df_waves_long$waves)) +
-# #     ggplot(df_waves_long, aes(x=df_waves_long$att, y=df_waves_long$delta)) +
-#     ggplot(df_waves_long, aes(x=value)) + geom_histogram(colour="white") +
-#  
-# #     ggplot(df_waves_long, aes(x=med, y=value)) +
-# #         geom_line(aes(colour=waves,size=waves)) +
-# #         geom_point(aes(colour=waves), size=1)+
-# #           geom_line(size=1)
-#         
-# #         geom_histogram(colour="black", binwidth=5) +
-# #         facet_grid(waves ~ .) +
-#         facet_wrap(CurrentActivity~waves) +
-# #         facet_wrap(CurrentActivity~waves, scales="free") +
-#         ggtitle("EEG waves distribution") +
-#         theme(legend.position="none")  
-# 
-# wavesHIST
+# -- transforming df to plot using face_wrap
+df_waves_long = melt(df[,meltVar], id.vars = xVar, variable.name = "index", value.name = "s");
 
-# p1 = vectorOfPlots[[1]]; p2 = vectorOfPlots[[2]]; p3 = vectorOfPlots[[3]]; p4 = vectorOfPlots[[4]];
-# p5 = vectorOfPlots[[5]]; p6 = vectorOfPlots[[6]]; p7 = vectorOfPlots[[7]]; p8 = vectorOfPlots[[8]];
-# 
-# pdf("~/aiworker.com.R/output/Att_vs_Waves.pdf")
-# grid.arrange(wavesHIST, ncol=1, nrow=1)
-# dev.off()
-# 
-# # getwd()
-# setwd(function_dir)
-# source("multiplot.R")
-# multiplot(wavesHIST)
-# multiplot(vectorOfPlots[[1]],vectorOfPlots[[2]], vectorOfPlots[[3]], vectorOfPlots[[4]], cols=2)
-# 
-# 
-# # grid.arrange(vectorOfPlots1, vectorOfPlots2, vectorOfPlots3, vectorOfPlots4, ncol=2, nrow=2)   
-# grid.arrange(vectorOfPlots[[1]], vectorOfPlots[[2]], vectorOfPlots[[3]], vectorOfPlots[[4]], ncol=2, nrow=2)
-# 
-# # pdf("~/aiworker.com.R/output/Att_vs_Waves.pdf")
-# # grid.arrange(vectorOfPlots[[1]], vectorOfPlots[[2]], vectorOfPlots[[3]], vectorOfPlots[[4]],
-# #              vectorOfPlots[[5]], vectorOfPlots[[6]], vectorOfPlots[[7]], vectorOfPlots[[8]])
-# # dev.off()
-# 
-# # remove(vectorOfPlots)
+if(AggregationFlag){
+  df_waves_long_Total = df[,meltVar]; df_waves_long_Total$index = 's'; df_waves_long_Total$CurrentActivity = 'aggregate';
+  df_waves_long_Total = df_waves_long_Total[c(1,3,2)]
+  df_waves_long = rbind(df_waves_long, df_waves_long_Total)
+}
+if(TotalFlag){df_waves_long = df_waves_long_Total}
+
+# -- remove s=0
+# df_waves_long$s[df_waves_long$s==0] = NA;  df_waves_long$s = na.omit(df_waves_long$s)
+
+# -- crete s distribution graph
+p3 = ggplot(df_waves_long, aes(x=s)) + geom_histogram(colour="white") +
+  facet_wrap(xFacetWrap, scales = yScales, nrow = GraphRow, ncol = GraphCol) +
+  geom_vline(aes(xintercept=-30), colour="blue",linetype="solid",size=0.5) +
+  geom_vline(aes(xintercept=0), colour="red", linetype="solid",size=1) +
+  geom_vline(aes(xintercept=30), colour="blue",linetype="solid",size=0.5) +
+  ggtitle(gTitle) + 
+  theme(legend.position="none", 
+        plot.title = element_text(lineheight=1,size=18, face="bold"),
+        strip.text.x = element_text(size=TitletxtSize, face="bold"))
+
+# -- saving graph to png file
+png("~/aiworker.com.R/output/s_distribution.png", width=GraphWidth, height=800, res=120)
+p3
+dev.off()
 
 
-
-
-
-# -- loading data
-# load("~/aiworker.com.R/data/mental_states.RData")
-
-
-# df_hist_att = hist(eegData$high_alpha, 
-#                    breaks = 70, 
-#                    col="blue", xlab="wave", ylab="count", main="Distribution of delta")
-
-
-#   
-#     vectorOfPlots[[length( vectorOfPlots) + 1]] =
-# #   i=1
-# #   vectorOfPlots[[i]] <- 
-#       ggplot(df, aes(x = df$att, y = df[,wave_name[i]] )  ) +
-#           geom_point(size=1, colour="blue") +
-#           #           geom_line(size=1, colour="blue") +
-#           ggtitle(wave_name[i]) #+ xlab("iterations") + ylab("waves")
-# #           ggtitle(i) #+ xlab("iterations") + ylab("waves")
-
-# }
+# -- saving graph to png file
+if(TotalFlag)
+{
+  png("~/aiworker.com.R/output/total_a_m_s_distribution.png", width=800, height=800, res=120)
+  grid.arrange(p1,p2,p3, ncol=1, nrow=3)
+  dev.off()
+}
